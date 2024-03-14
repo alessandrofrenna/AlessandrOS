e3e126aa968f137ab58038f10cc736877fcc36b9
Alessandro Frenna <alessandrofrenna95@gmail.com>
2024-03-14 16:14:03 +0100

    Add gnome-mutter-patched bling

---
+++ modules/bling/installers/gnome-mutter-patched.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+wget -O "/etc/yum.repos.d/_copr_gnome-mutter-patched.repo" "https://copr.fedorainfracloud.org/coprs/trixieua/mutter-patched/repo/fedora-${OS_VERSION}/trixieua-mutter-patched-fedora-${OS_VERSION}.repo"
+
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland gdm
+
+rm -f /etc/yum.repos.d/_copr_gnome-mutter-patched.repo
