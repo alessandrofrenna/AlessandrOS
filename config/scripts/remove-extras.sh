889ce96fcf8ed8448f5fdd75d0c3f14a9a27d8c7
alessandrofrenna <alessandrofrenna95@gmail.com>
2024-04-26 18:09:03 +0200

    remove-extras.sh splitt remove on multiple line

--- config/scripts/remove-extras.sh
+++ config/scripts/remove-extras.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-rpm-ostree uninstall htop yelp gnome-extensions-app || true
+rpm-ostree uninstall htop || true
+rpm-ostree uninstall yelp|| true
+rpm-ostree uninstall gnome-extensions-app || true
